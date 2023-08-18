0c512fd6d0b6dbdb44a2f46054e184b0e5d368d8
AR-Shahin <mdshahinmije96@gmail.com>
2023-08-18 21:45:06 +0600

    add requirements.txt file

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Slider(models.Model):
+    title = models.CharField(max_length=100,name="title")
+    content = models.TextField(name="content")
+    image = models.ImageField(name="image")
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'sliders'
+        managed = True
+        verbose_name = 'Slider'
+        verbose_name_plural = 'Sliders'
