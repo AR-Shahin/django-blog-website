c0397d11c787013f307c5fb2d22283af80cba8b3
AR-Shahin <mdshahinmije96@gmail.com>
2023-08-19 12:20:19 +0600

    frontend migrations complete

--- frontend/admin.py
+++ frontend/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register([Slider,Category])
+admin.site.register([Slider,Category,SubCategory,Tag,Post])
--- frontend/models.py
+++ frontend/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Slider(models.Model):
+    id = models.AutoField(primary_key=True,name="id",unique=True)
     title = models.CharField(max_length=100,name="title")
+    order = models.IntegerField(name="order",default=0)
+    status = models.BooleanField(name="status",default=True)
     content = models.TextField(name="content")
-    image = models.ImageField(name="image",upload_to='images/')
+    image = models.ImageField(name="image",upload_to='images/sliders/')
 
 
     def __str__(self):
@@ -29,6 +32,21 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'categories'
         
+class SubCategory(models.Model):
+    title = models.CharField(max_length=100,name="title")
+    slug = models.CharField(max_length=100,name="slug")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name="sub_categories")
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'sub_categories'
+        managed = True
+        verbose_name = 'Sub Category'
+        verbose_name_plural = 'sub_categories'
+        
 class Tag(models.Model):
     name = models.CharField(max_length=100,name="name")
     slug = models.CharField(max_length=100,name="slug")
@@ -45,12 +63,17 @@ class Tag(models.Model):
         
         
 class Post(models.Model):
-    title = models.CharField(max_length=100,name="title")
-    slug = models.CharField(max_length=100,name="slug")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name="posts")
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE,related_name="posts",null=True)
+    tags = models.ManyToManyField(Tag,related_name="posts")
+    title = models.CharField(max_length=100,name="title",unique=True)
+    slug = models.CharField(max_length=100,name="slug",unique=True,primary_key=True)
     view = models.IntegerField(default=0)
     status = models.BooleanField(default=True)
+    image = models.ImageField(name="image",upload_to='images/posts/',null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    content = models.TextField()
+    description = models.TextField()
+
 
 
     def __str__(self):
